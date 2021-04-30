782298d26a02d34633c4f1ecd84c132a6725a73c
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-04-30 16:22:36 +0545

    Index Page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Search This | Minimal Search Engine</title>
+    <link rel="icon" href="assets/images/icons/search.png">
+</head>
+<body>
+<div class="wrapper indexPage">
+
+
+    <div class="mainSection">
+
+        <div class="logoContainer">
+            <img src="assets/images/SearchThisLogo.png">
+        </div>
+
+
+        <div class="searchContainer">
+
+            <form action="search.php" method="GET">
+
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+
+
+            </form>
+
+        </div>
+
+
+    </div>
+
+
+</div>
+</body>
+</html>
