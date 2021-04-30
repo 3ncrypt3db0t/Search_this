9a5bbf3d07a7251cc0122b9b8fdd04559244c9ab
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-04-30 16:02:45 +0545

    Search Page

---
+++ search.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Search This | Minimal Search Engine</title>
+    <link rel="icon" href="assets/images/icons/search.png">
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerContent">
+        <div class="logoContainer">
+            <a href="index.php">
+                <img src="assets/images/SearchThisLogo.png">
+            </a>
+        </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
