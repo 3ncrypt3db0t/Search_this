e8e4aac7fb9a3df03aace8723ff2957ca14f20c1
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-03 13:27:38 +0545

    Crawl Page

--- crawl.php
+++ crawl.php
@@ -1,7 +1,8 @@
 <?php
+include("classes/DomDocumentParser.php");
 
 function followLinks($url) {
-    
+    $parser = new DomDocumentParser($url);
 }
 
 $startURL = "https://3ncrypt3db0t.github.io/";
