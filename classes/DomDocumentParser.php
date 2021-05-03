1fd9d078b4560fe5c692fc06e3cb6e1a38521a07
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-03 18:52:12 +0545

    Classes | DomDocumentParser Class

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -1,8 +1,21 @@
 <?php
 
 class DomDocumentParser {
+
+    private $doc;
+
     public function __construct($url) {
         echo "URL: $url";
+
+        $options = array(
+            'http'=>array('method'=>"GET", 'header'=>"User-Agent: searchthisBot/1.0\n")
+        );
+
+        $context = stream_context_create($options);
+
+        $this->doc = new DomDocument();
+
+        @$this->doc->loadHTML(file_get_contents($url, false, $context));
     }
 }
 
