a92e38972f2eb9001de4eacd50901782a3ac8a1d
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-03 19:00:34 +0545

    Crawl Page

--- crawl.php
+++ crawl.php
@@ -3,6 +3,12 @@ include("classes/DomDocumentParser.php");
 
 function followLinks($url) {
     $parser = new DomDocumentParser($url);
+
+    $linkList = $parser->getLinks();
+
+    foreach($linkList == $link) {
+        $href = $link->getAttribute("href");
+    }
 }
 
 $startURL = "https://3ncrypt3db0t.github.io/";
