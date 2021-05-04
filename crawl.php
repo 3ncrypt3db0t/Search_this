b69c735f6a8697f0d8ce5d033e48280dedcfe022
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-04 16:36:36 +0545

    Crawl Page

--- crawl.php
+++ crawl.php
@@ -4,88 +4,92 @@ include("classes/DomDocumentParser.php");
 $alreadyCrawled = array();
 $crawling = array();
 
-
 function createLink($src, $url) {
 
-    $scheme = parse_url($url)["scheme"]; // Scheme = http || https
-    $host = parse_url($url)["host"] ; // Host = www.bbc.com
-    
-    if(substr($src, 0, 2) == "//") {
-        $src = $scheme . ":" . $src;
-    }
-    else if(substr($src, 0, 1) == "/") {
-        $src = $scheme . "://" . $host .$src;
-    }
-    else if(substr($src, 0, 2) == "./") {
-        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substring($src, 1);
-    }
-    else if(substr($src, 0, 3) == "../") {
-        $src = $scheme . "://" . $host . "/" . $src;
-    }
-    else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
-        $src = $scheme . "://" . $host . "/" . $src;
-    }
+	$scheme = parse_url($url)["scheme"]; // SCHEME: http || //https
+	$host = parse_url($url)["host"]; // HOST: www.bbc.com
+	
+	if(substr($src, 0, 2) == "//") {
+		$src =  $scheme . ":" . $src;
+	}
+	else if(substr($src, 0, 1) == "/") {
+		$src = $scheme . "://" . $host . $src;
+	}
+	else if(substr($src, 0, 2) == "./") {
+		$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+	}
+	else if(substr($src, 0, 3) == "../") {
+		$src = $scheme . "://" . $host . "/" . $src;
+	}
+	else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+		$src = $scheme . "://" . $host . "/" . $src;
+	}
 
-    return $src;
+	return $src;
 }
 
 function getDetails($url) {
-    $parser = new DomDocumentParser($url);
-    
-    $titleArray = $parser->getTitleTags();
 
-    if(sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
-        return;
-    }
+	$parser = new DomDocumentParser($url);
 
-    $title = $titleArray->item(0)->nodeValue;
-    $title = str_replace("\n", "", $title);
+	$titleArray = $parser->getTitleTags();
 
-    if($title == "") {
-        return;
-    }
-    echo "URL: $url, Title: $title";
+	if(sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
+		return;
+	}
+
+	$title = $titleArray->item(0)->nodeValue;
+	$title = str_replace("\n", "", $title);
+
+	if($title == "") {
+		return;
+	}
+	echo "URL: $url, Title: $title<br>";
 
 }
 
 function followLinks($url) {
 
-    global $alreadyCrawled;
-    global $crawling;
+	global $alreadyCrawled;
+	global $crawling;
 
-    $parser = new DomDocumentParser($url);
+	$parser = new DomDocumentParser($url);
 
-    $linkList = $parser->getLinks();
+	$linkList = $parser->getLinks();
 
-    foreach($linkList as $link) {
-        $href = $link->getAttribute("href");
+	foreach($linkList as $link) {
+		$href = $link->getAttribute("href");
 
-        if(strpos($href, "#") !== false) {
-            continue;
-        } 
-        else if(substr($href, 0, 11) == "javascript:") {
-            continue;
-        } 
+		if(strpos($href, "#") !== false) {
+			continue;
+		}
+		else if(substr($href, 0, 11) == "javascript:") {
+			continue;
+		}
 
-        $href = createLink($href, $url);
 
-        if(in_array($href, $alreadyCrawled)) {
-            $alreadyCrawled[] = $href;
-            $crawling[] = $href;
+		$href = createLink($href, $url);
 
-            getDetails($href);
-        }
-        else return;
 
-    }
+		if(!in_array($href, $alreadyCrawled)) {
+			$alreadyCrawled[] = $href;
+			$crawling[] = $href;
 
-    array_shift($crawling);
+			getDetails($href);
+		}
+		else return;
 
-    foreach($crawling as $site) {
-        followLinks($site);
-    }
+
+	}
+
+	array_shift($crawling);
+
+	foreach($crawling as $site) {
+		followLinks($site);
+	}
+
 }
 
-$startURL = "https://bbc.com";
-followLinks($startURL);
+$startUrl = "http://www.bbc.com";
+followLinks($startUrl);
 ?>
