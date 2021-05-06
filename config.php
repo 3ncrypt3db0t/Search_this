5a7d5b0875e3a663e9c8ea78571a34471a10ff33
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-06 09:27:13 +0545

    DB Config Page

--- config.php
+++ config.php
@@ -4,10 +4,11 @@ ob_start();
 
 try {
 
+    $con = new PDO("mysql:dbname=searchThis;host=localhost","root", "");
+
 }
 catch(PDOException $e) {
-
+    echo "Connection Failed:" . $e->getMessage();
 }
 
 ?>
-
