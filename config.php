c3974e761ed5ab57bf259e62856f71443ebd67be
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-06 10:02:06 +0545

    DB Config

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
-
 ob_start();
 
 try {
 
-    $con = new PDO("mysql:dbname=searchThis;host=localhost","root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+	$con = new PDO("mysql:dbname=searchThis;host=localhost", "root", "");
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
-catch(PDOException $e) {
-    echo "Connection Failed:" . $e->getMessage();
+catch(PDOExeption $e) {
+	echo "Connection failed: " . $e->getMessage();
 }
-
 ?>
