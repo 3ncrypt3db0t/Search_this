d3d7a8a9386929861b3162bc9ff006dc297ea67a
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-06 09:00:40 +0545

    Index Page

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <head>
 	<title>SearchThis | Minimal SE</title>
 
+	<meta charset="UTF-8">
+  	<meta name="description" content="Search the Web for sites, images and videos">
+  	<meta name="keywords" content="Search Engine, SearchThis, websites">
+  	<meta name="author" content="3ncrypt3db0t">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	<link rel="icon" href="assets/images/icons/search.png">
 
