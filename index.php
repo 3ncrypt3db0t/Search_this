fa360f47a09a4f6770018cfd56a5c78c11b9faa2
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-06 11:06:20 +0545

    Index Page

--- index.php
+++ index.php
@@ -3,11 +3,10 @@
 <head>
 	<title>SearchThis | Minimal SE</title>
 
-	<meta charset="UTF-8">
-  	<meta name="description" content="Search the Web for sites, images and videos">
-  	<meta name="keywords" content="Search Engine, SearchThis, websites">
-  	<meta name="author" content="3ncrypt3db0t">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="Search engine, searchthis, websites">
+	<meta name="author" content="Encrypt3db0t">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 	<link rel="icon" href="assets/images/icons/search.png">
@@ -31,6 +30,7 @@
 
 					<input class="searchBox" type="text" name="term">
 					<input class="searchButton" type="submit" value="Search">
+
 
 				</form>
 
