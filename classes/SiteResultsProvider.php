87ce178195864ea7dca8f03c773560919bb30650
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-12 12:18:21 +0545

    Class | SiteResultsProvider

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -8,7 +8,14 @@ class SiteResultsProvider {
     }
 
     public function getNumResults($term) {
-        $query = $this->con->prepare("")
+        $query = $this->con->prepare("SELECT COUNT(*) as total
+                                FROM sites WHERE title LIKE :term
+                                OR url LIKE :term
+                                OR keywords LIKE :term
+                                OR description LIKE :term");
+
+        
+
     }
 }
 ?>
