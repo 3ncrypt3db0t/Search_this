085be649b985186dad58974b67d586c690080b43
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-12 12:27:16 +0545

    Class | SearchResultsProvider

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -16,7 +16,11 @@ class SiteResultsProvider {
 
         $searchTerm = "%". $term . "%";
         $query->bindParam(":term", $searchTerm);
+        $query->execute();
 
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return $row["total"];
+        
     }
 }
 ?>
