207cf72e1b76f61e885af9bd123896afb4cb88f3
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-05-13 18:35:44 +0545

    Class | SiteResultsProvider

--- classes/SiteResultsProvider.php
+++ classes/SiteResultsProvider.php
@@ -1,50 +1,61 @@
-<?php 
+<?php
 class SiteResultsProvider {
 
-    private $con;
+	private $con;
 
-    public function __construct($con) {
-        $this->con = $con;
-    }
+	public function __construct($con) {
+		$this->con = $con;
+	}
 
-    public function getNumResults($term) {
-        $query = $this->con->prepare("SELECT COUNT(*) as total
-                                FROM sites WHERE title LIKE :term
-                                OR url LIKE :term
-                                OR keywords LIKE :term
-                                OR description LIKE :term");
+	public function getNumResults($term) {
 
-        $searchTerm = "%". $term . "%";
-        $query->bindParam(":term", $searchTerm);
-        $query->execute();
+		$query = $this->con->prepare("SELECT COUNT(*) as total 
+										 FROM sites WHERE title LIKE :term 
+										 OR url LIKE :term 
+										 OR keywords LIKE :term 
+										 OR description LIKE :term");
 
-        $row = $query->fetch(PDO::FETCH_ASSOC);
-        return $row["total"];
+		$searchTerm = "%". $term . "%";
+		$query->bindParam(":term", $searchTerm);
+		$query->execute();
 
-    }
+		$row = $query->fetch(PDO::FETCH_ASSOC);
+		return $row["total"];
 
-    public function getResultsHtml($page, $pageSize, $term) {
-        $query = $this->con->prepare("SELECT *
-                                FROM sites WHERE title LIKE :term
-                                OR url LIKE :term
-                                OR keywords LIKE :term
-                                OR description LIKE :term
-                                ORDER BY clicks DESC");
+	}
 
-        $searchTerm = "%". $term . "%";
-        $query->bindParam(":term", $searchTerm);
-        $query->execute();
+	public function getResultsHtml($page, $pageSize, $term) {
 
-        $resultsHtml = "<div class='siteResults'>";
+		$query = $this->con->prepare("SELECT * 
+										 FROM sites WHERE title LIKE :term 
+										 OR url LIKE :term 
+										 OR keywords LIKE :term 
+										 OR description LIKE :term
+										 ORDER BY clicks DESC");
 
-        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-            $title = row["title"];
-            $resultsHtml .= "$title <br>";
-        }
+		$searchTerm = "%". $term . "%";
+		$query->bindParam(":term", $searchTerm);
+		$query->execute();
 
-        $resultsHtml .= "</div>"
 
-        return $resultsHtml;
-    }
+		$resultsHtml = "<div class='siteResults'>";
+
+
+		while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+
+			$title = $row["title"];
+			$resultsHtml .= "$title <br>";			
+
+		}
+
+
+		$resultsHtml .= "</div>";
+
+		return $resultsHtml;
+	}
+
+
+
+
 }
 ?>
