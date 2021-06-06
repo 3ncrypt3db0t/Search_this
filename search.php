15f719d1adfe54bef6f119c244f3470a0323070b
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-06-06 09:33:57 +0545

    Search Page

--- search.php
+++ search.php
@@ -3,168 +3,177 @@ include("config.php");
 include("classes/SiteResultsProvider.php");
 
 if(isset($_GET["term"])) {
-$term = $_GET["term"];
+	$term = $_GET["term"];
 }
 else {
-exit("You must enter a search term");
+	exit("You must enter a search term");
 }
 
 $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 $page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 
-
+	
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>BoodlE | Minimal SE</title>
-<link rel="shortcut icon" href="assets/images/icons/search.png" type="image/x-icon">
+	<title>BoodlE | Simple Search</title>
 
-<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<link rel="shortcut icon" href="assets/images/icons/search.png" type="image/x-icon">
 
-<script 
-	src="https://code.jquery.com/jquery-3.6.0.slim.min.js" 
-	integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" 
-	crossorigin="anonymous">
-</script>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
 </head>
 <body>
 
-<div class="wrapper">
+	<div class="wrapper">
+	
+		<div class="header">
 
-	<div class="header">
 
+			<div class="headerContent">
 
-		<div class="headerContent">
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/boodleLogo.png">
+					</a>
+				</div>
 
-			<div class="logoContainer">
-				<a href="index.php">
-					<img src="assets/images/boodleLogo.png">
-				</a>
+				<div class="searchContainer">
+
+					<form action="search.php" method="GET">
+
+						<div class="searchBarContainer">
+
+							<input class="searchBox" type="text" name="term" value="<?php echo $term; ?>">
+							<button class="searchButton">
+								<img src="assets/images/icons/search.png">
+							</button>
+						</div>
+
+					</form>
+
+				</div>
+
 			</div>
 
-			<div class="searchContainer">
 
-				<form action="search.php" method="GET">
+			<div class="tabsContainer">
 
-					<div class="searchBarContainer">
+				<ul class="tabList">
 
-						<input class="searchBox" type="text" name="term" value="<?php echo $term; ?>">
-						<button class="searchButton">
-							<img src="assets/images/icons/glass.png">
-						</button>
-					</div>
+					<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
 
-				</form>
+					<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
+
 
 			</div>
-
 		</div>
 
 
-		<div class="tabsContainer">
-
-			<ul class="tabList">
-
-				<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
-					<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
-						Sites
-					</a>
-				</li>
-
-				<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
-					<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
-						Images
-					</a>
-				</li>
-
-			</ul>
-
-
-		</div>
-	</div>
-
-
-	<div class="mainResultsSection">
-
-		<?php
-		$resultsProvider = new SiteResultsProvider($con);
-		$pageSize = 20;
-
-		$numResults = $resultsProvider->getNumResults($term);
-
-		echo "<p class='resultsCount'>$numResults results found</p>";
 
 
 
-		echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
-		?>
-
-
-	</div>
 
 
 
-	<div class="paginationContainer">
-
-		<div class="pageButtons">
 
 
-
-			<div class="pageNumberContainer">
-				<img src="assets/images/pageStart.png">
-			</div>
+		<div class="mainResultsSection">
 
 			<?php
+			$resultsProvider = new SiteResultsProvider($con);
+			$pageSize = 20;
 
-			$pagesToShow = 10;
-			$numPages = ceil($numResults / $pageSize);
-			$pagesLeft = min($pagesToShow, $numPages);
+			$numResults = $resultsProvider->getNumResults($term);
 
-			$currentPage = $page - floor($pagesToShow / 2);
-
-			if($currentPage < 1) {
-				$currentPage = 1;
-			}
-
-			if($currentPage + $pagesLeft > $numPages + 1) {
-				$currentPage = $numPages + 1 - $pagesLeft;
-			}
-
-			while($pagesLeft != 0 && $currentPage <= $numPages) {
-
-				if($currentPage == $page) {
-					echo "<div class='pageNumberContainer'>
-							<img src='assets/images/pageSelected.png'>
-							<span class='pageNumber'>$currentPage</span>
-						</div>";
-				}
-				else {
-					echo "<div class='pageNumberContainer'>
-							<a href='search.php?term=$term&type=$type&page=$currentPage'>
-								<img src='assets/images/page.png'>
-								<span class='pageNumber'>$currentPage</span>
-							</a>
-					</div>";
-				}
-
-
-				$currentPage++;
-				$pagesLeft--;
-
-			}
+			echo "<p class='resultsCount'>$numResults results found</p>";
 
 
 
-
-
+			echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
 			?>
 
-			<div class="pageNumberContainer">
-				<img src="assets/images/pageEnd.png">
+
+		</div>
+
+
+
+		<div class="paginationContainer">
+
+			<div class="pageButtons">
+
+
+
+				<div class="pageNumberContainer">
+					<img src="assets/images/pageStart.png">
+				</div>
+
+				<?php
+
+				$pagesToShow = 10;
+				$numPages = ceil($numResults / $pageSize);
+				$pagesLeft = min($pagesToShow, $numPages);
+
+				$currentPage = $page - floor($pagesToShow / 2);
+
+				if($currentPage < 1) {
+					$currentPage = 1;
+				}
+
+				if($currentPage + $pagesLeft > $numPages + 1) {
+					$currentPage = $numPages + 1 - $pagesLeft;
+				}
+
+				while($pagesLeft != 0 && $currentPage <= $numPages) {
+
+					if($currentPage == $page) {
+						echo "<div class='pageNumberContainer'>
+								<img src='assets/images/pageSelected.png'>
+								<span class='pageNumber'>$currentPage</span>
+							</div>";
+					}
+					else {
+						echo "<div class='pageNumberContainer'>
+								<a href='search.php?term=$term&type=$type&page=$currentPage'>
+									<img src='assets/images/page.png'>
+									<span class='pageNumber'>$currentPage</span>
+								</a>
+						</div>";
+					}
+
+
+					$currentPage++;
+					$pagesLeft--;
+
+				}
+
+
+
+
+
+				?>
+
+				<div class="pageNumberContainer">
+					<img src="assets/images/pageEnd.png">
+				</div>
+
+
+
 			</div>
+
 
 
 
@@ -173,17 +182,10 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 
 
+
+
+
 	</div>
-
-
-
-
-
-
-
-</div>
-
-<script src="assets/js/script.js"></script>
-
+	<script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
