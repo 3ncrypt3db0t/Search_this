d3e7bf28ee4a0630da4dfa0acc0dc6f98e7345db
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-06-06 09:03:11 +0545

    Assets | JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
     
     $(".result").on("click", function () {
         
+        var id = $(this).attr("data-linkId");
         var url = $(this).attr("href");
-        console.log(url);
+
+        if(!id) {
+            alert("data-linkId attribute not found");
+        }
 
         return false;
     });
