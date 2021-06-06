1f0d4a96118a2d00c62f565b69e433adbfc2e0ce
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-06-06 09:26:37 +0545

    Assets | JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,34 @@
 $(document).ready(function() {
-    
-    $(".result").on("click", function () {
-        
-        var id = $(this).attr("data-linkId");
-        var url = $(this).attr("href");
 
-        if(!id) {
-            alert("data-linkId attribute not found");
-        }
 
-        increaseLinkClicks(id, url);
+	$(".result").on("click", function() {
+		
+		var id = $(this).attr("data-linkId");
+		var url = $(this).attr("href");
 
-        return false;
-    });
+		if(!id) {
+			alert("data-linkId attribute not found");
+		}
+
+		increaseLinkClicks(id, url);
+
+		return false;
+	});
+
 
 });
 
+
 function increaseLinkClicks(linkId, url) {
-    $.post("ajax/updateLinkCount.php", {linkId:linkId});
+
+	$.post("ajax/updateLinkCount.php", {linkId: linkId})
+	.done(function(result) {
+		if(result != "") {
+			alert(result);
+			return;
+		}
+
+		window.location.href = url;
+	});
+
 }
