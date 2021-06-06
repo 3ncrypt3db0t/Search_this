e7e8efd73c6fed3dc4baf3e350dde6fe80749586
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-06-06 08:52:18 +0545

    Assets | JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
     
     $(".result").on("click", function () {
         
+        var url = $(this).attr("href");
+        console.log(url);
+
         return false;
     });
 
 });
+
+function increaseLinkClicks(linkId, url) {
+
+}
