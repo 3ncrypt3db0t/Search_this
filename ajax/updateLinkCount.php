83e6f67f581a008e16e5b0d0cc7f8789d92f7d67
1DI0T.1RL <48232101+3ncrypt3db0t@users.noreply.github.com>
2021-06-06 09:26:03 +0545

    Ajax | LinkCountUpdate

--- ajax/updateLinkCount.php
+++ ajax/updateLinkCount.php
@@ -1,15 +1,13 @@
-<?php 
-
+<?php
 include("../config.php");
 
-if(isset($_POST("linkId"))) {
-    $query = $con->prepare("UPDATE sites SET clicks = clicks + 1 WHERE id=:id");
-    $query->bindParam(":id", $_POST["linkId"]);
+if(isset($_POST["linkId"])) {
+	$query = $con->prepare("UPDATE sites SET clicks = clicks + 1 WHERE id=:id");
+	$query->bindParam(":id", $_POST["linkId"]);
 
-    $query->execute();
+	$query->execute();
 }
 else {
-    echo "No link passed to page";
+	echo "No link passed to page";
 }
-
 ?>
